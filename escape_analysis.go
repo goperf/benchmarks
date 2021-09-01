cede182dbe19a485a1008bd9dc29e23120121a09
srimal_n <srimal@pickme.lk>
2021-09-01 23:08:53 +0530

    minor improvements

--- escape_analysis.go
+++ escape_analysis.go
@@ -5,3 +5,9 @@ func EscapeAnalysis() []int {
 	var data = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 	return data
 }
+
+//go:noinline
+func EscapeAnalysisOptimized() [10]int {
+	var data = [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	return data
+}
